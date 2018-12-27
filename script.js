a2fbe42a7bb408a474a4881e79ea0d9e16dcba56
5alidz <kzack-dev@hotmail.com>
2018-12-27 19:57:26 +0300

    exploring

--- script.js
+++
@@ -1,6 +0,0 @@
-const { __data, __templates } = window
-
-const $ = id => document.getElementById(id)
-const removeIds = elements => elements.map(el => el.removeAttribute('id'))
-const removeId = (element, id) => element.removeAttribute(id)
-
