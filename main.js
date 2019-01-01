e1e392074811129140f2b664ab671b6a12182697
5alidz <kzack-dev@hotmail.com>
2019-01-01 22:38:52 +0300

    with themes

--- main.js
+++ main.js
@@ -1,25 +1,74 @@
 const log = console.log.bind(console)
-
-const templates = window.templates || {}
-const target = document.getElementById('target')
-
-const tabs = Array.from(document.querySelectorAll('.tab'))
-
-const activeTab = tabs
-  .filter(tab => tab.classList.contains('active-tab'))[0]
-  .innerHTML
-  .toLowerCase()
-target.innerHTML = templates[activeTab]
-
 const removeClasses = (arr, _class) => {
   arr.map(el => el.classList.remove(_class))
 }
+const templates = window.templates || {}
+// setup dom elements.
+const target = document.getElementById('target')
+const tabs = Array.from(document.querySelectorAll('.tab'))
+const getActiveTab = () => tabs
+  .filter(tab => tab.classList.contains('active-tab'))[0]
+  .innerHTML
+  .toLowerCase()
+const themeBtn = document.getElementById('theme-btn')
+const currentThemeDOM = document.getElementById('current-theme')
+const indicator = document.getElementById('theme-indicator')
+const tabsContainer = document.querySelectorAll('.tabs-container')[0]
+const activeTab = () => document.querySelectorAll('.active-tab')[0]
+
+let currentTheme = 'light'
+// handle theme
+themeBtn.onclick = () => {
+  if(currentTheme === 'light'){
+    const all = Array.from(document.querySelectorAll('.light'))
+    currentTheme = 'dark'
+    currentThemeDOM.innerHTML = 'Light'
+    indicator.classList.remove('fa-moon')
+    indicator.classList.add('fa-sun')
+    tabsContainer.classList.add('tabs-brdr-dark')
+    tabsContainer.classList.remove('tabs-brdr-light')
+    activeTab().classList.add('active-tab-dark')
+    activeTab().classList.remove('active-tab-light')
+    target.innerHTML = templates[getActiveTab()](currentTheme)
+    all.map(el => {
+      el.classList.remove('light')
+      el.classList.add('dark')
+    })
+    return
+  }
+  if(currentTheme === 'dark'){
+    const all = Array.from(document.querySelectorAll('.dark'))
+    currentTheme = 'light'
+    // update theme button
+    currentThemeDOM.innerHTML = 'Dark'
+    indicator.classList.remove('fa-sun')
+    indicator.classList.add('fa-moon')
+    tabsContainer.classList.remove('tabs-brdr-dark')
+    tabsContainer.classList.add('tabs-brdr-light')
+    activeTab().classList.remove('active-tab-dark')
+    activeTab().classList.add('active-tab-light')
+    // update tab content
+    target.innerHTML = templates[getActiveTab()](currentTheme)
+    // update classes
+    all.map(el => {
+      el.classList.remove('dark')
+      el.classList.add('light')
+    })
+    return
+  }
+}
+// handle tabs
+target.innerHTML = templates[getActiveTab()](currentTheme)
+
 tabs.map(tab => {
   tab.onclick = (e) => {
     const content = e.target.innerHTML.toLowerCase()
-    const template = templates[content]
+    const template = templates[content](currentTheme)
     target.innerHTML = template
     removeClasses(tabs, 'active-tab')
+    removeClasses(tabs, 'active-tab-dark')
+    removeClasses(tabs, 'active-tab-light')
     tab.classList.add('active-tab')
+    tab.classList.add(`active-tab-${currentTheme}`)
   }
 })
--- templates.js
+++ templates.js
@@ -1,33 +1,55 @@
 window.templates = {
-  about: `
-  <div class="tab-content">
-    <p>hello, my name is khaled zakaria and i'm a web developer based in riyadh saudi arabia.</p>
+  about: theme => `
+  <div class="enter ${theme}">
+    <p>hello 👋, my name is khaled zakaria and i'm a  web-developer based in riyadh saudi arabia.</p>
     <br>
-    <p>i love helping people, contact me via email or twitter.</p>
+    <p></p>
+    <br>
+    <h2 class='pd-b'>Links</h2>
+    <ul class='pd-l'>
+      <li>
+        <div class='flex align-c mr-b'>
+          <i class='fab fa-twitter white mr-r pd-all round brd-all-${theme}' style='color:#38A1F3'></i>
+          <a href="https://twitter.com/_5alidz" class='${theme}-link'>Twitter</a>
+        </div>
+       </li>
+      <li>
+        <div class='flex align-c mr-b'>
+          <i class='fab fa-github  white mr-r pd-all round brd-all-${theme}' style='color: #333'></i>
+          <a href="https://github.com/5alidz" class='${theme}-link'>Github</a>
+        </div>
+      </li>
+      <li>
+        <div class='flex align-c mr-b'>
+          <i class='fab fa-youtube  white mr-r pd-all round brd-all-${theme}' style='color: #FF0000'></i>
+          <a href="mailto:kzack-dev@hotmail.com" class='${theme}-link'>Youtube</a>
+        </div>
+      </li>
+      <li>
+        <div class='flex align-c mr-b'>
+          <i class='far fa-envelope  white mr-r pd-all round brd-all-${theme}' style='color: #333'></i>
+          <a href="mailto:kzack-dev@hotmail.com" class='${theme}-link'>Email</a>
+        </div>
+      </li>
+    </ul>
   </div>
   `.trim(),
-  projects: ``.trim(),
-  blog: ``.trim(),
-  links: `
-  <ul class='links-container tab-content'>
-    <li>
-      <div class='link'>
-        <i class='fab fa-twitter' style='color: #38A1F3'></i>
-        <a href="https://twitter.com/_5alidz">Twitter</a>
+  projects: theme => `
+  <div class='enter ${theme}'>
+    <div class='grid ${theme}'>
+      <h3>Title.</h3>
+      <div class='flex align-c'>
+        <p>Consectetur saepe sit ea facere totam? Aspernatur vero delectus assumenda Consectetur saepe sit ea facere totam? Aspernatur vero delectus assumenda</p>
+        <img src="https://picsum.photos/100/100" alt="">
       </div>
-     </li>
-    <li>
-      <div class='link'>
-        <i class='fab fa-github' style='color: #333'></i>
-        <a href="https://github.com/5alidz">Github</a>
+      <div class='flex'>
+        <a href="" class='mr-r ${theme}-link'>Code</a>
+        <a href="" class='mr-l ${theme}-link'>Live demo</a>
       </div>
-    </li>
-    <li>
-      <div class='link'>
-        <i class='fas fa-envelope' style='color: #333'></i>
-        <a href="mailto:kzack-dev@hotmail.com">Email</a>
-      </div>
-    </li>
-  </ul>
+    </div>
+  </div>
+  `.trim(),
+  blog: theme => `
+  <div class='enter ${theme}'>d</div>
   `.trim()
 }
